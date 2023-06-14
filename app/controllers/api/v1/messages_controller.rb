8037f6dfe780a14a2843ea683177aa84bd08182b
Hienphan0111 <hienphan@digitalcad.edu.vn>
2023-06-14 09:53:36 +0700

    Fix lint err

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::MessagesController < ApplicationController
     num_rand = rand(1..5)
     message = Message.find(num_rand)
     if message
-      render json: { status: 'SUCCESS', message: "Fetch random message successfully", data: message }, status: :ok
+      render json: { status: 'SUCCESS', message: 'Fetch random message successfully', data: message }, status: :ok
     else
       render json: message.errors, status: :bad_request
     end
